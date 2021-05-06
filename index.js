2c602759872bb5d91f4dfea408d2c69755e3d934
Rshokar <ravindershokar@hotmail.com>
2021-05-06 16:46:58 -0700

    Added component to post Posts to db from client. Also began devleoping Post class which has update and delete meathods. Added post route in index.js to add Post to DB.

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {
-    readFile
-} = require('fs');
-const {
-    MongoClient
-} = require('mongodb');
-const {
-    read
-} = require('fs/promises');
+const { readFile } = require('fs');
+const { MongoClient } = require('mongodb');
+const { read } = require('fs/promises');
 
 const app = express();
 
@@ -16,41 +10,45 @@ app.use("/js", express.static("static/js"));
 app.use("/css", express.static("static/css"));
 app.use("/html", express.static("static/html"));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+/**
+ * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+ * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+ */
+const uri = "mongodb+srv://testing:gcX9e2D4a4HXprR0@sellery.4rqio.mongodb.net/Sellery?retryWrites=true&w=majority"
+
+// Mongo DB Client.
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
 //Connect App to DB. 
 async function main() {
-    /**
-     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-     */
-    const uri = "mongodb+srv://testing:gcX9e2D4a4HXprR0@sellery.4rqio.mongodb.net/Sellery?retryWrites=true&w=majority"
+  try {
+    // Connect to the MongoDB cluster
+    await client.connect();
 
-    const client = new MongoClient(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
-
-    try {
-        // Connect to the MongoDB cluster
-        await client.connect();
-
-        // Make the appropriate DB calls
-        await listDatabases(client);
-
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
+  } catch (e) {
+    console.error(e);
+  }
+  //finally {
+  //  await client.close();
+  //}
 }
 
 main().catch(console.error);
 
 
 async function listDatabases(client) {
-    databasesList = await client.db().admin().listDatabases();
+  databasesList = await client.db().admin().listDatabases();
 
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+  console.log("Databases:");
+  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
 };
 
 /**
@@ -59,12 +57,12 @@ async function listDatabases(client) {
  * @date April-29-2021
  */
 app.get("/", (req, res) => {
-    readFile("static/html/index.html", "utf-8", (err, html) => {
-        if (err) {
-            res.status(500).send("sorry, out of order");
-        }
-        res.send(html);
-    })
+  readFile("static/html/index.html", "utf-8", (err, html) => {
+    if (err) {
+      res.status(500).send("sorry, out of order");
+    }
+    res.send(html);
+  })
 })
 
 
@@ -75,12 +73,12 @@ app.get("/", (req, res) => {
  * @date April-30-2021
  */
 app.get("/template", (req, res) => {
-    readFile("static/html/template.html", "utf-8", (err, html) => {
-        if (err) {
-            res.status(500).send("Sorry, out of order.");
-        }
-        res.send(html);
-    })
+  readFile("static/html/template.html", "utf-8", (err, html) => {
+    if (err) {
+      res.status(500).send("Sorry, out of order.");
+    }
+    res.send(html);
+  })
 })
 
 /**
@@ -89,12 +87,12 @@ app.get("/template", (req, res) => {
  * @date April-30-2021
  */
 app.get("/post_summary", (req, res) => {
-    readFile("static/html/post_summary.html", "utf-8", (err, html) => {
-        if (err) {
-            res.status(500).send("Sorry, out of order.");
-        }
-        res.send(html);
-    })
+  readFile("static/html/post_summary.html", "utf-8", (err, html) => {
+    if (err) {
+      res.status(500).send("Sorry, out of order.");
+    }
+    res.send(html);
+  })
 })
 
 /**
@@ -103,12 +101,12 @@ app.get("/post_summary", (req, res) => {
  * @date April-30-2021
  */
 app.get("/post", (req, res) => {
-    readFile("static/html/post.html", "utf-8", (err, html) => {
-        if (err) {
-            res.status(500).send("Sorry, out of order.");
-        }
-        res.send(html);
-    })
+  readFile("static/html/post.html", "utf-8", (err, html) => {
+    if (err) {
+      res.status(500).send("Sorry, out of order.");
+    }
+    res.send(html);
+  })
 })
 
 /**
@@ -117,12 +115,12 @@ app.get("/post", (req, res) => {
  * @date April-30-2021
  */
 app.get("/feed", (req, res) => {
-    readFile("static/html/feed.html", "utf-8", (err, html) => {
-        if (err) {
-            res.status(500).send("Sorry, out of order.");
-        }
-        res.send(html);
-    })
+  readFile("static/html/feed.html", "utf-8", (err, html) => {
+    if (err) {
+      res.status(500).send("Sorry, out of order.");
+    }
+    res.send(html);
+  })
 })
 
 /**
@@ -131,13 +129,74 @@ app.get("/feed", (req, res) => {
  * @author Mike Lim 
  * @date April-30-2021
  */
- app.get("/storefront", (req, res) => {
-    readFile("static/html/storefront.html", "utf-8", (err, html) => {
-        if (err) {
-            res.status(500).send("Sorry, out of order.");
-        }
-        res.send(html);
+app.get("/storefront", (req, res) => {
+  readFile("static/html/storefront.html", "utf-8", (err, html) => {
+    if (err) {
+      res.status(500).send("Sorry, out of order.");
+    }
+    res.send(html);
+  })
+})
+
+
+
+/**
+ * This route will post data to Mongo DB. 
+ * @author Ravinder Shokar 
+ * @date May-05-2021
+ */
+app.post("/post_post", (req, res) => {
+
+  let post = req.body;
+
+  // Only for this route. 
+  const db = client.db("sellery");
+
+  db.collection("post").insertOne(post)
+    .then((result) => {
+      let obj = {
+        status: "success",
+        message: "message",
+      }
+      console.log(obj);
+      res.send(obj);
     })
+    .catch((err) => {
+      let obj = {
+        result: {},
+        status: "error",
+        message: "",
+      }
+      console.log(obj);
+      res.send(obj);
+    })
+})
+
+
+/**
+ * This route is responsible for updating post in with post data. 
+ * @author Ravinder Shokar
+ * @version 1.0
+ * @date May 06 2021
+ */
+app.post("/update_post", (req, res) => {
+  post = req.body;
+
+  const db = client.db("sellary");
+
+  db.collection("post").updateOne(
+    { _id: post.ID },
+    {
+      $set: {
+        title: post.title,
+        description: post.description,
+        units: post.uinits,
+        price: post.price,
+        quantity: post.quantity,
+      }
+    }
+  );
+  console.log(post);
 })
 
 app.listen(8000, () => console.log("App available on http://localhost:8000"));
---
+++ static/js/post_post.js
@@ -0,0 +1,35 @@
+/**
+ * This component will post data to the node server. Will return an obj with 
+ * message, status, data. 
+ * 
+ * eg. 
+ * obj = {
+ *  data: data,
+ *  message: "Hello Anyone",
+ *  status: "success"
+ * } 
+ * 
+ * @author Ravinder Shokar 
+ * @version 1.0 
+ * @date May 06 2021
+ */
+
+function postOnePost(post) {
+  $.ajax({
+    url: "/post_post",
+    type: "POST",
+    dataType: "JSON",
+    data: post,
+    success: (data) => {
+      return data
+    },
+    error: (err) => {
+      let obj = {
+        status: "error",
+        message: "Error posting data",
+        error: err,
+      }
+      return obj;
+    }
+  })
+}
