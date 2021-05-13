96f9747631085b69780c74b45bde2cfb3a11cbb8
Jimun Jang <wkdwlans009@gmail.com>
2021-05-13 15:06:12 -0700

    added cookie, token, userVerification for every route, styled some for storefront page

--- static/js/login_post.js
+++ static/js/login_post.js
@@ -1,8 +1,34 @@
-const form = document.querySelector('form');
-form.addEventListener('submit', (e) => {
+/** 
+ * AJAX call for login. 
+ * it sends the data of email and password input.
+ * 
+ * @author Jimun Jang
+ * @date May 10, 2021
+*/
+const form = $('form')[0];
+const error = $('.error')[0];
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    error.textContent = '';
 
     const email = form.email.value;
     const password = form.password.value;
 
-    console.log(email, password);
-});
+    $.ajax({
+        url: '/login',
+        dataType: "json",
+        type: "POST",
+        data: {
+            email,
+            password
+        },
+        success: function (data) {
+            window.location.href = '/template';
+        },
+        error: function (data) {
+            error.textContent = data.responseJSON.error;
+        }
+    })
+})
